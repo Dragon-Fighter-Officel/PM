a6a45e4c184add7f686b8d28d042eb33b1a8d0a0
Dragon-Fighter-Officel <parhammerzaii3@gmail.com>
2025-02-21 19:25:40 +0330

    1.1

---
+++ Java/login.js
@@ -0,0 +1,29 @@
+function Facebook()
+{
+  alert("Soon");
+}
+function BtnlogIn()
+{
+  var UserName = document.getElementById("username").value;
+  var Password = document.getElementById("password").value;
+  if (Password=="W100P")
+  {
+    Win = window.open("Pages/index.html");
+  }
+  else 
+  {
+    alert("false")
+  }
+}
+function Openwhatsapp()
+{
+  Win = window.open("https://wa.me/qr/ALOQNKMRECTJP1","_blank");
+}
+function Opentelegram()
+{
+  Win = window.open("http://t.me/Dragon_fighter","_blank");
+}
+function Openinstagram()
+{
+  Win = window.open("https://www.instagram.com/dragon_fighter_officel/?igsh=MmZsZzdpejZxMnh0#","_blank");
+}
