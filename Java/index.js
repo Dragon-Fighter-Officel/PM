6e017ca6e8a6bac2b18cc336a786e1f010fc4307
Dragon-Fighter-Officel <parhammerzaii3@gmail.com>
2025-06-12 14:39:32 +0330

    v.1.2

--- Java/index.js
+++ Java/index.js
@@ -1,5 +1,5 @@
 var divv = document.querySelector(".Wallpaper");
 var total = 20;
 for (var i = 5; i <= total; i++) {
-    divv.innerHTML+=`<a href="imges/dragon-fighter-officel.github.ioPM (${i}).jpg" download><article class="imges-1"><img class="img-1" src="imges/dragon-fighter-officel.github.ioPM (${i}).jpg" alt="img"><p>برای دانلود کلیک کنید</p></article></a>`
+    divv.innerHTML+=`<a href="imges/dragon-fighter-officel.github.ioPM (${i}).jpg" download><article class="imges-1"><img class="img-1" src="imges/dragon-fighter-officel.github.ioPM (${i}).jpg" alt="img"><p> Click for download </p></article></a>`
 }
